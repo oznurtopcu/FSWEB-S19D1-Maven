99e7553680a791a153d99673a0270bd734445b9e
oznurtopcu <topcuoznur121@gmail.com>
2025-04-21 22:37:30 +0300

    added validations to entity classes

--- src/main/java/com/workintech/s18d2/entity/Fruit.java
+++ src/main/java/com/workintech/s18d2/entity/Fruit.java
@@ -1,6 +1,9 @@
 package com.workintech.s18d2.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,12 +16,17 @@ import lombok.NoArgsConstructor;
 public class Fruit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    @Min(value=0, message = "ID değeri 0'dan küçük olamaz!")
     private long id;
 
     @Column(name="name")
+    @NotNull
+    @NotBlank
     private String name;
 
-    @Column(name="name")
+    @Column(name="price")
+    @NotNull
     private double price;
 
     @Column(name="fruit_type")
--- src/main/java/com/workintech/s18d2/entity/Vegetable.java
+++ src/main/java/com/workintech/s18d2/entity/Vegetable.java
@@ -1,6 +1,9 @@
 package com.workintech.s18d2.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,14 +16,19 @@ import lombok.NoArgsConstructor;
 public class Vegetable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Min(0)
     private long id;
 
     @Column(name="name")
+    @NotNull
+    @NotBlank
     private String name;
 
     @Column(name="price")
+    @NotNull
     private double price;
 
     @Column(name="is_grown_on_tree")
+    @NotNull
     private boolean isGrownOnTree;
 }
